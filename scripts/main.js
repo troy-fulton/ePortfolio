1cee2f432a4dd18a46b0bf311d15eb0f9c1e081a
troy-fulton <troyfulton@tamu.edu>
2018-05-13 22:27:29 -0500

    Oh, Nothing

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// My first actual JS file
+var myHeading = document.querySelector('p');
+myHeading.textContent = 'Hello world!';
+
+var friends = ["Sarah", "Bill", "Lisa", 4];
+
+friends.push("Maya");
+
+alert(friends);
+alert("Math.floor(8.8) = " + Math.floor(8.8));
